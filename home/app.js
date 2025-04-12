90b2a2a7226758d6d4082069d078a58bcc442561
missinggoverner <zekariyasamdu24@gmail.com>
2025-04-12 23:06:26 +0300

    oh look a loader

--- home/app.js
+++ home/app.js
@@ -1,20 +1,31 @@
-import { displayTrendy } from "./utils.js";
+ import { displayTrendy } from "./utils.js";
+
 
 const entrance = document.querySelector('.fa-bars');
 const tredning = document.querySelector('.fa-fire');
 const search = document.querySelector('.search')
+const trendContainer = document.querySelector('.trend');
+const loader = document.querySelector('.loader');
+const range = document.querySelector('#range');
+const perviousBtn = document.querySelector('.pre-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let period = range.value;
+// Adds the animation and dipalys trends and search
 entrance.addEventListener('click', ()=>{
-    tredning.style.display = 'flex';
+    trendContainer.style.display = 'flex'
     search.style.display = 'flex';
     search.classList.add('entrance')
-    tredning.classList.add('entrance');
+    trendContainer.classList.add('entrance');
 
 })
 
+
 tredning.addEventListener('click', ()=>{
+    loader.style.display = 'flex';
     const getData = async () => {
         try {
-            let res = await fetch('https://discoveryprovider.audius.co/v1/tracks/trending?app_name=my_music_app');
+            let res = await fetch(`https://discoveryprovider.audius.co/v1/tracks/trending?time_range=${period}&app_name=my_music_app`);
             if(res.ok){
                 let data = await res.json();
                 displayTrendy(data); 
@@ -25,6 +36,9 @@ tredning.addEventListener('click', ()=>{
         } 
         catch(error){
             console.error('Error', error);
+        }
+        finally{
+            loader.style.display = 'none';
         }
     }
 getData();
