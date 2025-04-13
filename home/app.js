0fc51e3444b314bd3d89f01667a1e0c8182ff126
missinggoverner <zekariyasamdu24@gmail.com>
2025-04-13 16:52:39 +0300

    new layout

--- home/app.js
+++ home/app.js
@@ -1,45 +1,57 @@
- import { displayTrendy } from "./utils.js";
+import { displayTrendy } from "./utils.js";
 
 
-const entrance = document.querySelector('.fa-bars');
-const tredning = document.querySelector('.fa-fire');
 const search = document.querySelector('.search')
-const trendContainer = document.querySelector('.trend');
 const loader = document.querySelector('.loader');
-const range = document.querySelector('#range');
+const title = document.querySelector('.title-container');
 const perviousBtn = document.querySelector('.pre-btn');
 const nextBtn = document.querySelector('.next-btn');
 
-let period = range.value;
-// Adds the animation and dipalys trends and search
-entrance.addEventListener('click', ()=>{
-    trendContainer.style.display = 'flex'
-    search.style.display = 'flex';
-    search.classList.add('entrance')
-    trendContainer.classList.add('entrance');
+// Adding Even Listening to ranges
+const week = document.querySelector('.week');
+const month = document.querySelector('.month');
+const year = document.querySelector('.year');
 
-})
+let ranges = [week, month, year];
 
+ranges.forEach(range => {
 
-tredning.addEventListener('click', ()=>{
-    loader.style.display = 'flex';
-    const getData = async () => {
-        try {
-            let res = await fetch(`https://discoveryprovider.audius.co/v1/tracks/trending?time_range=${period}&app_name=my_music_app`);
-            if(res.ok){
-                let data = await res.json();
-                displayTrendy(data); 
+    range.addEventListener('click', () => {
+        loader.style.display = 'flex';
+        let period = range.innerHTML;
+        const getData = async () => {
+            try {
+                let res = await fetch(`https://discoveryprovider.audius.co/v1/tracks/trending?time=${period}&app_name=my_music_app`);
+                if (res.ok) {
+                    let data = await res.json();
+                    displayTrendy(data);
+                }
+                if (!res.ok) {
+                    console.log('erroe', res)
+                }
             }
-            if(!res.ok){
-                console.log('erroe', res)
+            catch (error) {
+                console.error('Error', error);
             }
-        } 
-        catch(error){
-            console.error('Error', error);
+            finally {
+                loader.style.display = 'none';
+                title.innerHTML = `${period}'s tending`;
+            }
         }
-        finally{
-            loader.style.display = 'none';
-        }
+        getData();
+
+    })
+});
+
+
+// adding a border when scroling to the windows 
+
+let header = document.querySelector('.headers');
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > 0) {
+        header.classList.add('scroll');
+    }else{
+        header.classList.remove('scroll');
     }
-getData();
 })
